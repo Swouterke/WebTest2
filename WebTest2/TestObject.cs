e4ab1db08059121996a943f5704d170291dbf38d
LoQutwouterus NV <wvercruysse45@gmai.comLoQutus@live.com>
2018-09-14 14:19:30 +0200

    Add project files.

---
+++ WebTest2/TestObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebTest2
+{
+    public class TestObject
+    {
+        public int Count { get; set; }
+        public string Title { get; set; }
+    }
+}
